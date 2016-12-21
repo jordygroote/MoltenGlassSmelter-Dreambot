26279959eb7b97340d96271133a7d3671c148ae2
Jordy Groote <jordygroote@live.nl>
2016-12-22 00:33:45 +0100

    version 1.2 fixed some buggs

--- src/main/main.java
+++ src/main/main.java
@@ -9,11 +9,15 @@ import org.dreambot.api.wrappers.interactive.GameObject;
 import org.dreambot.api.wrappers.interactive.NPC;
 import org.dreambot.api.wrappers.widgets.WidgetChild;
 
+import java.util.concurrent.TimeUnit;
+
+import java.awt.*;
+
 /**
  * Created by jordygroote on 18-12-16.
  */
 
-@ScriptManifest(category = Category.MISC, name = "MoltenGlassSmelter", author = "Jordyownn", version = 1.0)
+@ScriptManifest(category = Category.CRAFTING, name = "MoltenGlassSmelter", author = "Jordyownn", version = 1.1)
 public class main extends AbstractScript {
 //    AREA
     Area BankArea = new Area(3098, 3494,3095,3497,0);
@@ -22,33 +26,42 @@ public class main extends AbstractScript {
     private final int BucketOfSand = 1783;
     private final int SodaAsh = 1781;
 
+    //GUI
+    private long timeBegan;
+    private long timeRan;
+    private int glassMade;
+
 
 //    STATE
     private int state = -1;
 
     @Override
     public void onStart() {
+        timeBegan = System.currentTimeMillis();
+        glassMade = 0;
         log("Welcome to MoltenGlassSmelter 1.0");
         state = 0;
     }
 
     @Override
     public int onLoop() {
-        if (state == 0) {
-            withdraw();
-        } else if (state == 1) {
-            smelt();
-        } else if (state == 2) {
-            bank();
+        if (getDialogues().canContinue()) {
+            getDialogues().continueDialogue();
+        } else {
+            if (state == 0) {
+                withdraw();
+            } else if (state == 1) {
+                smelt();
+            } else if (state == 2) {
+                bank();
+            }
         }
         return Calculations.random(500,1000);
     }
 
     private void withdraw(){
-        log("Check inventory is empty");
         if(getInventory().isEmpty()) {
             if (BankArea.contains(getLocalPlayer())) {
-                log("Inside bank area, opening bank.");
                 NPC banker = getNpcs().closest(npc -> npc != null && npc.hasAction("Bank"));
 
                 if (!getBank().isOpen()){
@@ -56,15 +69,7 @@ public class main extends AbstractScript {
 
                     }
                 } else {
-                    log("Sleep untill bank is open.");
                     if(sleepUntil(()-> getBank().open(),9000)) {
-                        log("Withdrawing bucket of sand.");
-//                        getBank().withdraw(BucketOfSand, 14);
-//                        sleep(Calculations.random(1000,2500));
-//                        getBank().withdraw(SodaAsh, 14);
-//                        sleep(Calculations.random(1000, 2000));
-//                        getBank().close();
-//                        state = 1;
                         if(getBank().withdraw(BucketOfSand, 14)){
                             if(sleepUntil(() -> getInventory().contains("Bucket of sand"), 2500)){
                                 if(getBank().withdraw(SodaAsh, 14)){
@@ -79,13 +84,11 @@ public class main extends AbstractScript {
                 }
 
             } else {
-                log("Not inside BankArea, walking to bank.");
                 if (getWalking().walk(BankArea.getCenter())) {
                     sleepUntil(() -> getLocalPlayer().distance(BankArea.getCenter()) < Calculations.random(2, 4), 7000);
                 }
             }
         } else {
-            log("Is not empty setting state to 1");
             state = 1;
         }
     }
@@ -94,14 +97,14 @@ public class main extends AbstractScript {
         if(getInventory().contains(BucketOfSand)){
             if(FurnaceArea.contains(getLocalPlayer())){
                 GameObject Furnace = getGameObjects().closest("Furnace");
-                if (Furnace != null) {
+                if (Furnace != null && getInventory().count("Molten glass") < 14) {
                     getInventory().get(BucketOfSand).useOn(Furnace);
-                    sleep(2000);
+                    sleep(3000);
                     if (!getWidgets().getWidgetChildrenContainingText("Molten glass").isEmpty()) {
                         WidgetChild child = getWidgets().getWidgetChildrenContainingText("Molten glass").get(0);
                         if (child != null) {
                             if(child.interact("Make All")){
-                                sleepWhile( () -> getInventory().contains(BucketOfSand), Calculations.random(35000, 40000));
+                                sleepWhile( () -> getInventory().contains(BucketOfSand) && !getDialogues().canContinue(), Calculations.random(35000, 40000));
                             }
                         }
                     }
@@ -122,15 +125,14 @@ public class main extends AbstractScript {
     }
 
     private void bank(){
-        log("State is now " + state + ", banking..");
         if(BankArea.contains(getLocalPlayer())) {
             NPC banker = getNpcs().closest(npc -> npc != null && npc.hasAction("Bank"));
             if (banker != null){
                 banker.interact("Bank");
                 sleep(Calculations.random(2000, 3000));
                 if(sleepUntil(()-> getBank().open(),9000)) {
-                    log("Deposite everything");
                     if(getBank().depositAllItems()){
+                        glassMade += 14;
                         if (sleepUntil(()-> !getInventory().isFull(),4000)){
                             state = 0;
                         }
@@ -144,8 +146,33 @@ public class main extends AbstractScript {
         }
     }
 
+    @Override
+    public void onPaint(Graphics graphics) {
+        timeRan = System.currentTimeMillis() - this.timeBegan;
+        graphics.drawString(ft(timeRan), 380, 300);
+        graphics.drawString("" + glassMade + " moltenglass made", 380, 315);
+        super.onPaint(graphics);
+    }
 
-
+    private String ft(long duration)
+    {
+        String res = "";
+        long days = TimeUnit.MILLISECONDS.toDays(duration);
+        long hours = TimeUnit.MILLISECONDS.toHours(duration)
+                - TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(duration));
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(duration)
+                - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS
+                .toHours(duration));
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(duration)
+                - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS
+                .toMinutes(duration));
+        if (days == 0) {
+            res = ("Time Ran:" + hours + ":" + minutes + ":" + seconds);
+        } else {
+            res = (days + ":" + hours + ":" + minutes + ":" + seconds);
+        }
+        return res;
+    }
 
     @Override
     public void onExit() {
